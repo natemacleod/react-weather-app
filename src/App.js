38c119808c16fec7032a24178661f0bdbfef4223
Nate MacLeod <virtualnatem+gh@gmail.com>
2022-06-24 11:50:08 -0400

    add forecast tables

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
 import PrimeReact from 'primereact/api';
-import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext'
 import { Menubar } from 'primereact/menubar';
+import { ScrollPanel } from 'primereact/scrollpanel';
 
 PrimeReact.ripple = true;
 
 const getWeatherIcon = i => {
   switch (i) {
     case 'snow': return (<i className='ib wi bi bi-snow' />);
-    case 'snow-showers-day', 'snow-showers-night': return (<i className='ib wi bi bi-cloud-snow-fill' />);
+    case 'snow-showers-day':
+    case 'snow-showers-night': return (<i className='ib wi bi bi-cloud-snow-fill' />);
     case 'rain': return (<i className='ib wi bi bi-cloud-rain-fill' />);
-    case 'thunder-rain', 'thunder-showers-day', 'thunder-showers-night': return (<i className='ib wi bi bi-cloud-lightning-rain-fill' />);
-    case 'showers-day', 'showers-night': return (<i className='ib wi bi bi-cloud-drizzle-fill' />);
+    case 'thunder-rain':
+    case 'thunder-showers-day':
+    case 'thunder-showers-night': return (<i className='ib wi bi bi-cloud-lightning-rain-fill' />);
+    case 'showers-day':
+    case 'showers-night': return (<i className='ib wi bi bi-cloud-drizzle-fill' />);
     case 'fog': return (<i className='ib wi bi bi-cloud-fog-fill' />);
     case 'wind': return (<i className='ib wi bi bi-wind' />);
     case 'cloudy': return (<i className='ib wi bi bi-clouds-fill' />);
@@ -29,17 +33,99 @@ const getWeatherIcon = i => {
   }
 }
 
-const formatDate = d => {
-  let arr = d.split('T');
-  arr[1] = arr[1].substring(0, arr[1].length - 3);
-  return arr[0] + " at " + arr[1];
+const formatDate = (d, timeFormat) => {
+  d = new Date(d);
+  switch (timeFormat) {
+    case 'NONE': 
+      return d.toLocaleString("en-US", {
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+      });
+    case 'HOUR':
+      return d.toLocaleString("en-US", {
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+      });
+    case 'FULL':
+      return d.toLocaleString("en-US", {
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "2-digit",
+        second: "2-digit"
+      });
+  }
 }
 
-const getDates = alert => {
-  let start = alert['onset'];
-  let end = alert['ends'];
+const getAlertDates = alert => {
+  let start = alert['onsetEpoch'] * 1000;
+  let end = alert['endsEpoch'] * 1000;
 
-  return formatDate(start) + " to " + formatDate(end);
+  return formatDate(start, 'FULL') + " to " + formatDate(end, 'FULL');
+}
+
+const getNextXHours = (days, x) => {
+  let hours = Array(x).fill(null);
+  let curDay = 0;
+  let curTime = new Date().getHours();
+  for (let i = 0; i < x; i++) {
+    hours[i] = days[curDay]['hours'][curTime];
+    curTime++;
+    if (curTime === 24) {
+      curTime = 0;
+      curDay++;
+    }
+  }
+  return hours;
+}
+
+const Forecast = (props) => {
+  const rows = props.data.map(d => {
+    return (
+      <tr className='dataRow' key={d['datetimeEpoch']}>
+        <td className='rh'>
+          <div className='date'>
+            <h5>{formatDate(d['datetimeEpoch'] * 1000, props.timeFormat)}</h5>
+            <div className='icontemp'>
+              {getWeatherIcon(d['icon'])}
+              <h3>{Math.round(d['temp'])}°</h3>
+            </div>
+          </div>
+        </td>
+        <td><em>{Math.round(d['feelslike'])}°</em></td>
+        <td>{Math.round(d['humidity'])}%</td>
+        <td>{Math.round(d['precipprob'])}%</td>
+        <td>{Math.round(d['windspeed'])} km/h</td>
+        <td>{d['uvindex']}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className='fc'>
+      <table className='heads'>
+        <thead>
+        <tr>
+          <th className='rh'></th>
+          <th>Feels Like</th>
+          <th>Humidity</th>
+          <th>POP</th>
+          <th>Wind Speed</th>
+          <th>UV Index</th>
+        </tr>
+        </thead>
+      </table>
+      <ScrollPanel style={{width: '100%', height: '400px'}}>
+        <table className='fctable'>
+          <tbody>{rows}</tbody>
+        </table>
+      </ScrollPanel>
+    </div>
+  );
 }
 
 const AlertView = (props) => {
@@ -49,7 +135,7 @@ const AlertView = (props) => {
         <i className='bi bi-exclamation-triangle' />
         <div className='alertText'>
           <h5 className="ib"><a href={a['link']} target='_blank'>{a['headline']}</a></h5>
-          <h6 className="ib"><em>{getDates(a)}</em></h6>
+          <h6 className="ib"><em>{getAlertDates(a)}</em></h6>
           <h6>{a['description']}</h6>
         </div>
       </div>
@@ -141,6 +227,13 @@ const LocationData = (props) => {
       {data['alerts'].length > 0 &&
         <AlertView alerts={data['alerts']} />
       }
+      <div id="forecast">
+        <Forecast data={getNextXHours(data['days'], 72)} timeFormat={'HOUR'} />
+        <Forecast data={data['days']} timeFormat={'NONE'} />
+      </div>
+      <div id='linkback'>
+        <p><a href="https://www.visualcrossing.com/">Weather Data Provided by Visual Crossing</a></p>
+      </div>
     </div>
   );
   else return (<div id='basics'>Loading</div>);
