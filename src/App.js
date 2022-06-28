39b180eb2eed57693d9e60e7e07e17e966c003a3
Nate MacLeod <virtualnatem+gh@gmail.com>
2022-06-28 16:12:39 -0400

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -22,65 +22,70 @@ PrimeReact.ripple = true;
 
 const App = (props) => {
     // State setup
-    const [active, setActive] = useState('Kitchener'); // Current location
+    const [active, setActive] = useState("~noLocations!!"); // Current location
     const [bar, setBar] = useState(""); // Contents of 'Add Location' bar
-    const [menuItems, setMenuItems] = useState([{
-        label: 'Kitchener',
-        command: () => setActiveLocation('Kitchener'),
-    }]); // List of locations for use in the Menubar component
+    const [menuItems, setMenuItems] = useState([]); // List of locations for use in the Menubar component
     const [resp, setResp] = useState(200); // HTTP status code of the most recent API request
     const [loading, setLoading] = useState(true); // Whether the app is loading (the app will not try to display API data when it is loading)
     const [data, setData] = useState({}); // All API data collected during runtime
 
-    // Whenever the active location changes, this will run
-    //   If we already have API data for the location, that will be used (this reduces unneeded API calls)
-    //   If we don't, new data will be fetched and converted to JSON
+    // Runs on initial render
     useEffect(() => {
-        if (active in data) {
+        addLocation('Kitchener');
+    }, [])
+
+    // Adds a location (from the add location bar) to the selection menu and makes it active
+    const addLocation = loc => {
+        let items = menuItems;
+        console.log(active);
+        items.push({
+            label: loc,
+            command: (e) => setActiveLocation(e.item.label)
+        });
+        setMenuItems(items);    
+        setActiveLocation(loc);
+        setBar("");
+    }
+
+    // Removes a location from the menu
+    const removeLocation = loc => {
+        let items = menuItems;
+        items = items.filter(i => i['label'] !== loc);
+        setMenuItems(items);
+    }
+
+    // Sets the active location to [loc]
+    // If we already have API data for the location, that will be used (this reduces unneeded API calls)
+    // If we don't, new data will be fetched and converted to JSON
+    const setActiveLocation = loc => {
+        setLoading(true);
+        if (loc in data) {
             setResp(200);
             setLoading(false);
-            return;
-        } else if (active == null) {
+        } else if (loc === "~noLocations!!") {
             setResp(1);
             setLoading(false);
-            return;
         } else fetch(
-            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${active}?unitGroup=metric&key=EVA3Q6YVR36WLKVSKA6LTUUR5&contentType=json&iconSet=icons2`,
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=metric&key=EVA3Q6YVR36WLKVSKA6LTUUR5&contentType=json&iconSet=icons2`,
             {
                 method: 'GET'
             }
         ).then((resp) => {
             if (!resp.ok) {
                 setResp(resp.status);
+                removeLocation(loc);
                 setLoading(false);
             } else {
                 setResp(200);
                 resp.json().then((j) => {
                     let d = data;
-                    d[active] = j;
+                    d[loc] = j;
                     setData(d);
                     console.log(j);
                     setLoading(false);
                 });
             }
         });
-    }, [active]);
-
-    // Adds a location (from the add location bar) to the selection menu and makes it active
-    const addLocation = () => {
-        let items = menuItems;
-        items.push({
-            label: bar,
-            command: () => setActiveLocation(bar),
-        });
-        setMenuItems(items);
-        setActiveLocation(bar);
-        setBar("");
-    }
-
-    // Changes the active location to [loc]
-    const setActiveLocation = loc => {
-        setLoading(true);
         setActive(loc);
     }
 
@@ -103,11 +108,12 @@ const App = (props) => {
                 end={
                     <div id="addLocation">
                         <InputText placeholder='Add a Location' value={bar} onChange={e => setBar(e.target.value)} />
-                        <Button icon="pi pi-plus" onClick={() => { addLocation() }} />
+                        <Button icon="pi pi-plus" onClick={() => { addLocation(bar) }} />
                     </div>
                 }
             />
-            {resp === 200 && !loading && <LocationData data={data[active]} loading={loading} />}
+            <p>{active}</p>
+            {resp === 200 && <LocationData data={data[active]} loading={loading} />}
             {resp !== 200 &&
                 <div id='e404'>
                     <h3>Error <strong>{resp}</strong></h3>
